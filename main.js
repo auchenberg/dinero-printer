0530437afa5001a198165cfb27bc14d1351e63df
Kenneth Auchenberg <kenneth@auchenberg.dk>
2017-11-25 11:45:06 -0800

    Handle case of no log

--- main.js
+++ main.js
@@ -10,46 +10,58 @@ const Options = require('./options')
 const ipc = electron.ipcMain
 
 let mb = menubar({
-  icon: path.join(electron.app.getAppPath(), '.icons', 'IconTemplate.png')
+    icon: path.join(electron.app.getAppPath(), '.icons', 'IconTemplate.png')
 })
 
 mb.on('ready', () => {
-  mb.tray.setContextMenu(
-    electron.Menu.buildFromTemplate([{
-      label: 'Preferences...',
-      click: () => mb.showWindow()
-    }, {
-      label: 'Quit',
-      click: () => electron.app.quit()
-    }])
-  )
+    mb.tray.setContextMenu(
+        electron.Menu.buildFromTemplate([
+            {
+                label: 'Preferences...',
+                click: () => mb.showWindow()
+            },
+            {
+                label: 'Quit',
+                click: () => electron.app.quit()
+            }
+        ])
+    )
 
-  this.options = new Options({
-    appName: mb.app.getName()
-  })
+    this.options = new Options({
+        appName: mb.app.getName()
+    })
 
-  this.printer = new Printer(this.options)
-  log.info('ready')
+    this.printer = new Printer(this.options)
+    log.info('ready')
 })
 
 ipc.on('updateOption', (evt, property, value) => {
-  log.info('updateOption.%s value=%s', property, value)
-  this.options[property] = value
+    log.info('updateOption.%s value=%s', property, value)
+    this.options[property] = value
 })
 
-ipc.on('getOptions', (evt) => {
-  evt.sender.send('optionsUpdated', this.options)
+ipc.on('getOptions', evt => {
+    evt.sender.send('optionsUpdated', this.options)
 })
 
-ipc.on('getLog', (evt) => {
-  var logString = ''
-  var stream = fs.createReadStream(log.findLogPath())
+ipc.on('getLog', evt => {
+    var logString = ''
+    var logPath = log.findLogPath()
 
-  stream.on('data', (chunk) => {
-    logString += chunk
-  })
+    fs.stat(logPath, (err, stats) => {
+        if (err) {
+            return
+        }
 
-  stream.on('end', () => {
-    evt.sender.send('logUpdated', logString)
-  })
+        var stream = fs.createReadStream(logPath)
+
+        stream.on('data', chunk => {
+            logString += chunk
+        })
+
+        stream.on('end', () => {
+            evt.sender.send('logUpdated', logString)
+        })
+        
+    })
 })
